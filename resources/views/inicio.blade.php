38cc28a8801d6580065935c172ab0ec285b64fea
Luis Angel Gonzalez Pelayo <luis.gonzalez7504@alumnos.udg.mx>
2021-07-05 05:17:52 -0500

    Proyecto Finalizado - Se agrego el diseño para el inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,6 +3,42 @@
 <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
     Inicio
 </h2>
+<div class="grid gap-6 mb-8 md:grid-cols-1">
+    <img src="{{ asset('img/redbull_bg.jpg') }}" class="object-contain md:object-scale-down">
+    <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+      <h4 class="mb-4 font-bold text-gray-600 dark:text-gray-300">
+        ¿Qu&eacute; es la FIA?
+      </h4>
+      <p class="text-gray-600 dark:text-gray-400">
+        La Federación Internacional del Automóvil conocida también como FIA (en francés, Fédération Internationale de l'Automobile), es una organización sin ánimo de lucro con sede en la Plaza de la Concordia de París, Francia, y que incluye 268 organizaciones automovilísticas de 143 países. Fundada en 1904, es mundialmente conocida por regular las competiciones de automovilismo más importantes del mundo, pero su ámbito de aplicación incluye todos los aspectos del automóvil, las carreteras, la movilidad, el medio ambiente o seguridad vial.
+      </p>
+    </div>
+    <div class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs">
+      <h4 class="mb-4 font-semibold">
+        Historia
+      </h4>
+      <p>
+        <ul>
+            <li>La Association Internationale des Automobile Clubs Reconnus (AIACR) fue fundada en París el 20 de junio de 1904.<br></li>
 
+            <li>En 1922, la FIA delegó la organización de carreras de automóviles a la Commission Sportive Internationale (CSI), un comité autónomo que más tarde se convirtió en la Fédération Internationale du Sport Automobile (FISA).<br></li>
+
+            <li>En 1950, organizaron el primer campeonato mundial automovilístico, actualmente conocido como la Fórmula 1.<br></li>
+
+            <li>En 1973, la FIA extendió su alcance para incluir carreras de rally; el Rally de Montecarlo fue el primer evento de ese tipo organizado por la FIA.<br></li>
+
+            <li>Una reestructuración de la FIA en 1993 dio lugar a la desaparición de la FISA, poniendo las carreras de coches bajo la administración directa de la FIA.<br></li>
+
+            <li>En 2011 la FIA fue reconocida por el Comité Olímpico Internacional por un período provisional de dos años e invitó a la misma que presentara una comisión de atletas para que esta fuese permanente.<br></li>
+
+            <li>En 2017 creó su Salón de la Fama.</li>
+        </ul>
+      </p>
+    </div>
+</div>
+<div class="grid gap-6 mb-8 md:grid-cols-2">
+    <div><img src="{{ asset('img/mclaren_bg.jpg') }}" class="object-contain"></div>
+    <div><img src="{{ asset('img/mercedes_bg.jpg') }}" class="object-contain"></div>
+</div>
 	
 @endsection
--- resources/views/layouts/sidebar-desktop.blade.php
+++ resources/views/layouts/sidebar-desktop.blade.php
@@ -8,86 +8,88 @@
         >
         FIA
         </a>
-        <ul class="mt-6">
-            <li class="relative px-6 py-3">
-                <!-- Active items have the snippet below -->
-                <!-- <span
-                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                aria-hidden="true"
-                ></span> -->
+        @auth        
+            <ul class="mt-6">
+                <li class="relative px-6 py-3">
+                    <!-- Active items have the snippet below -->
+                    <!-- <span
+                    class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                    aria-hidden="true"
+                    ></span> -->
 
-                <!-- Add this classes to an active anchor (a tag) -->
-                <!-- text-gray-800 dark:text-gray-100 -->
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="{{ asset('inicio') }}"
-                >
-                    <svg
-                        class="w-5 h-5"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+                    <!-- Add this classes to an active anchor (a tag) -->
+                    <!-- text-gray-800 dark:text-gray-100 -->
+                    <a
+                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ asset('inicio') }}"
                     >
-                        <path
-                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                        ></path>
-                    </svg>
-                    <span class="ml-4">Inicio</span>
-                </a>
-            </li>
-        </ul>
-        <ul>
-            <li class="relative px-6 py-3">
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="{{ route('escuderia.index') }}"
-                >
+                        <svg
+                            class="w-5 h-5"
+                            aria-hidden="true"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                            ></path>
+                        </svg>
+                        <span class="ml-4">Inicio</span>
+                    </a>
+                </li>
+            </ul>
+            <ul>
+                <li class="relative px-6 py-3">
+                    <a
+                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('escuderia.index') }}"
+                    >
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
+                        </svg>
+                        <span class="ml-4">Escuderias</span>
+                    </a>
+                </li>
+                <li class="relative px-6 py-3">
+                    <a
+                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('piloto.index') }}"
+                    >
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                        </svg>
+                        <span class="ml-4">Pilotos</span>
+                        
+                    </a>
+                </li>
+                <li class="relative px-6 py-3">
+                    <a
+                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('carrera.index') }}"
+                    >
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                        <span class="ml-4">Carreras</span>
+                    </a>
+                </li>
+                <li class="relative px-6 py-3">
+                    <a
+                    class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('archivo.index') }}"
+                    >
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    <span class="ml-4">Escuderias</span>
-                </a>
-            </li>
-            <li class="relative px-6 py-3">
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="{{ route('piloto.index') }}"
-                >
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-                    </svg>
-                    <span class="ml-4">Pilotos</span>
-                    
-                </a>
-            </li>
-            <li class="relative px-6 py-3">
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="{{ route('carrera.index') }}"
-                >
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                    </svg>
-                    <span class="ml-4">Carreras</span>
-                </a>
-            </li>
-            <li class="relative px-6 py-3">
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="{{ route('archivo.index') }}"
-                >
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                </svg>
-                    <span class="ml-4">Reclamos/Penalizaciones</span>
-                </a>
-            </li>
-        </ul>
+                        <span class="ml-4">Reclamos/Penalizaciones</span>
+                    </a>
+                </li>
+            </ul>
+        @endauth
         @guest
             <div class="px-6 my-6">
                 <a
